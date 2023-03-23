6b618f938cac91a78266ba3c0ece1c1dee95449c
BericMoeller <bamoeller@stu.naperville203.org>
2023-03-23 14:05:48 -0500

    Debugged enemyAI
    No compile or runtime errors but many many logical errors

--- PlantGameFinalProject/Assets/EnemyController.cs
+++ PlantGameFinalProject/Assets/EnemyController.cs
@@ -12,49 +12,55 @@ public class EnemyController : MonoBehaviour
     public Text healthStat;
     public Collider2D targetedPlayer;
     public bool wandering;
+    public int dirFacing;
     public Vector2 dirWalking;
     public float MOVEMENT_SPEED;
     public Vector2 playerChasingVector = new Vector2(0,0);
     public int ticksSinceLastScan = 0;
-    private int paranoiaLevel; //on scale from 0 to 99
-    // Start is called before the first frame update
+    private int paranoiaLevel;
 
     void Start()
     {
         gameObject.tag = "Enemy";
-        paranoiaLevel = 30;
+        paranoiaLevel = 150; //decreasing this increases the speed that scans occur
+        //^ I can modify this later so it triggers more often when players are being loud
         maxHealth = 100;
         health = maxHealth + 0;
-        MOVEMENT_SPEED = 0.1;
+        MOVEMENT_SPEED = 0.1F;
         debuffs = new List<string[]>();
+        dirFacing = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
         healthStat.text = ""+health;
     }
+
     void FixedUpdate(){
-        MakeMovementDecision();
+        MakeMovementDecision(); //all of this runs once per tick
         ticksSinceLastScan++;
     }
-    public float GetHealth(){
+
+    public float GetHealth(){ //returns the health values
         return health;
     }
-    public void ChangeHealth(int healthModifier){
+
+    public void ChangeHealth(int healthModifier){ //for like funky things in the future dunno
         health += healthModifier;
     }
-    public void Attacked(float damage, string debuff = "", int debuffTime = 0){
+
+    public void Attacked(float damage, string debuff = "", int debuffTime = 0){ // enemy is attacked(by player)
         health -= (int)damage;
         if(debuffTime != 0){
-            Debug.Log("Doing stuff");
-        } 
+        }
     }
 
-    RaycastHit2D VisionCheck(bool override = false, Vector2 scanAngle = Vector2.zero){
-        Vector2 angle;
-        if(!override){
-            switch(dirFacing){
+
+    RaycastHit2D CheckVision(bool changePower = false, Vector2 scanAngle = default)
+    { //checks vision at angle or direction facing
+        Vector2 angle; // this method refused to method so i don't like it.
+        if(!changePower){ //it's a bloody simple one too. i don't know why.
+            switch(dirFacing){// update: it is 12:45 am. i am awake and staring at this. help me. pain.
                 case 0:
                     angle = Vector2.up;
                     break;
@@ -76,36 +82,39 @@ public class EnemyController : MonoBehaviour
         }else{
             angle = scanAngle;
         }
-        RaycastHit2D colliderHit = Raycast(transform.position, angle);
-        return colliderHit;
+        RaycastHit2D colliderHit = Physics2D.Raycast(transform.position, angle); //gets the collider directly in front of
+        return colliderHit; //returns
     }
-    RaycastHit2D[] CheckWalkingIntoSomething() //returns empty array if not walking into something, array with ranked distances if yes
+
+    RaycastHit2D[] CheckWalkingIntoSomething() //returns null if not walking into something, array with distances if yes
     {
         RaycastHit2D[] hitsArray;
-        //IComparer reverseComparer = new myReverserClass();
-        RaycastHit2D colliderHit = VisionCheck();
+        RaycastHit2D colliderHit = CheckVision();
         if(colliderHit.distance < 1F){
             hitsArray = new RaycastHit2D[4];
             int origDirFacing = 0+ dirFacing;
-            for(int i = 0; i < 4; i++){
+            for(int i = 0; i < 4; i++){ //checks all directions and throws it in a funky lil' array
                 dirFacing = i;
-                RaycastHit2D colliderHit = VisionCheck();
+                colliderHit = CheckVision();
                 hitsArray[i] = colliderHit;
             }
             dirFacing = origDirFacing;
+            return hitsArray;
+        }else{
+            return null;
         }
-        return hitsArray;
     }
-    void MakeMovementDecision(){
+
+    void MakeMovementDecision(){ // BIG METHOD that controls all movement
         if(targetedPlayer == null){
-            RaycastHit2D colliderHit = VisionCheck();
-            if(colliderHit.collider.tag == "Player"){
-                targetedPlayer = Raycast.collider;
-            }else{
-                RaycastHit2D[] hits = CheckWalkingIntoSomething()[0];
+            RaycastHit2D colliderHit = CheckVision();
+            if(colliderHit.collider.tag == "Player"){ // checks for player
+                targetedPlayer = colliderHit.collider;
+            }else{ // usual result- does normal walking around and vibing
+                RaycastHit2D[] hits = CheckWalkingIntoSomething();
                 if(hits != null){
                     float largestDist = 0F;
-                    int largestIndex;
+                    int largestIndex = -1;
                     for(int i = 0; i < 4; i++){
                         if(hits[i].distance > largestDist){
                             largestIndex = i;
@@ -115,16 +124,22 @@ public class EnemyController : MonoBehaviour
                     dirFacing = largestIndex;
                     dirWalking = ChangeDirection();
                 }
-                if(ticksSinceLastScan > 150 && Random.Range(0,100)<paranoiaLevel){
+                if(ticksSinceLastScan > paranoiaLevel){ //scans regurally for the player(scans more commonly when paranoid)
                     ScanMode();
+                    ticksSinceLastScan = 0;
                 }
-                transform.position += dirWalking * MOVEMENT_SPEED;
+                transform.position += (Vector3)(dirWalking * MOVEMENT_SPEED);
             }
         }else{
-            ChasePlayer();
+            ChasePlayer(); // if the enemy sees the player it goes for that (yuppers)
         }
     }
-    Vector2 ChangeDirection(Vector2 direction = dirFacing){
+
+    Vector2 ChangeDirection(int direction = -1){ //converts my dumb dirFacing variable to the smart Vector2 dirWalking value
+        if(direction == -1){
+            direction = dirFacing;
+        }
+        Vector2 dirWalkingExit;
         switch(direction){
             case 0:
                 dirWalkingExit = Vector2.up;
@@ -144,17 +159,18 @@ public class EnemyController : MonoBehaviour
         }
         return dirWalkingExit;
     }
-    void ChasePlayer(){
+
+    void ChasePlayer(){ // Chases player(duh)
         Vector3 relPos = targetedPlayer.transform.localPosition;
         Vector2 playerChasingVector = relPos;
-        RaycastHit2D colliderHit = VisionCheck(true);
-        if(colliderHit.collider.tag != "Player"){
+        RaycastHit2D colliderHit = CheckVision(true);
+        if(colliderHit.collider.tag != "Player"){ // well if it's going to the player it doesn't have to worry about running into walls does it
             if(colliderHit.distance < 1){
                 int bestScore = 0;
-                int bestScoreIndex;
+                int bestScoreIndex = -1;
                 int score;
                 RaycastHit2D[] hitsArray = CheckWalkingIntoSomething();  
-                for(int i = 0; i < 4; i++){
+                for(int i = 0; i < 4; i++){ //rates each direction and goes to the best one
                     score = RateMovementDirection(hitsArray[i].distance, playerChasingVector, ChangeDirection(i));
                     if(bestScore < score){
                         bestScore = score;
@@ -165,22 +181,24 @@ public class EnemyController : MonoBehaviour
                 dirWalking = ChangeDirection();
             }
         }
-        transform.position += dirWalking * MOVEMENT_SPEED;
+        transform.position += (Vector3)(dirWalking * MOVEMENT_SPEED);
     }
-    int RateMovementDirection(float distance, Vector2 idealDirection, Vector2 direction){
+
+    int RateMovementDirection(float distance, Vector2 idealDirection, Vector2 direction){ //rates the quality of the direction
         int score = 0;
-        if(Mathf.Sign(dirWalkingTest.x)==Mathf.Sign(playerChasingVector.x)){
+        if(Mathf.Sign(direction.x)==Mathf.Sign(idealDirection.x)){ //points go to having the same direction, and a higher distance to the wall
             score += 20;
-        }if(Mathf.Sign(dirWalkingTest.y)==Mathf.Sign(playerChasingVector.y)){
+        }if(Mathf.Sign(direction.y)==Mathf.Sign(idealDirection.y)){
             score += 20;
         }
         score += (int)(distance)*5;
         return score;
     }
-    void ScanMode(){
+
+    void ScanMode(){ //well. Scans.
         RaycastHit2D itemScanned;
-        for(float i = 0; i < 2*Mathf.PI; i += Mathf.PI*0.2){
-            itemScanned = VisionCheck(true, i);
+        for(float i = 0; i < 2F*Mathf.PI; i += Mathf.PI*0.2F){
+            itemScanned = CheckVision(true, new Vector2(Mathf.Cos(i),Mathf.Sin(i)));
             if(itemScanned.collider.tag == "Player"){
                 targetedPlayer = itemScanned.collider;
                 break;
